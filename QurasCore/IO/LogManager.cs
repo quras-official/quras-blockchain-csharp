ce812af84836d3fcd6c4e0e1eff78846ca2f092a
krozy <krozy0820@hotmail.com>
2020-07-18 17:40:48 +0800

    - LogManager for QurasCore added

---
+++ QurasCore/IO/LogManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Quras.IO.Logger
+{
+    public static class LogManager
+    {
+        public static void PrintErrorLogs(StreamWriter writer, Exception ex)
+        {
+            string nowDateTime = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss");
+            writer.WriteLine(nowDateTime + ex.GetType());
+            writer.WriteLine(ex.Message);
+            writer.WriteLine(ex.StackTrace);
+            if (ex is AggregateException ex2)
+            {
+                foreach (Exception inner in ex2.InnerExceptions)
+                {
+                    writer.WriteLine();
+                    PrintErrorLogs(writer, inner);
+                }
+            }
+            else if (ex.InnerException != null)
+            {
+                writer.WriteLine();
+                PrintErrorLogs(writer, ex.InnerException);
+            }
+        }
+
+        public static void WriteExceptionLogs(Exception ex)
+        {
+            using (FileStream fs = new FileStream("error.log", FileMode.Create, FileAccess.Write, FileShare.None))
+            using (StreamWriter w = new StreamWriter(fs))
+            {
+                LogManager.PrintErrorLogs(w, ex);
+            }
+        }
+
+        public static void WriteString(string str)
+        {
+            using (FileStream fs = new FileStream("note.log", FileMode.Append, FileAccess.Write, FileShare.None))
+            using (StreamWriter w = new StreamWriter(fs))
+            {
+                w.WriteLine(str);
+            }
+        }
+    }
+}
